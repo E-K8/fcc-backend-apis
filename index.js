dac2d37e081f2b477add4bf4dc0fbdb992294afc
E-K8 <ekatplat@gmail.com>
2024-02-19 21:31:35 +0000

    get timestamp when given a string query or when no string is given

--- index.js
+++ index.js
@@ -25,12 +25,26 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api', function (req, res) {
+  let now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString(),
+  });
+});
+
 app.get('/api/:date', function (req, res) {
   console.log('request =>', req.params, '<= request');
   let dateString = req.params.date;
-  console.log(dateString);
-
-  res.json({ error: 'Invalid Date' });
+  let passedInValue = new Date(dateString);
+  if (passedInValue == 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({
+      unix: passedInValue.getTime(),
+      utc: passedInValue.toUTCString(),
+    });
+  }
 });
 
 // listen for requests :)
